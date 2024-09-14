371a68ce4c2d3a6c569393bcda8daf7688b61d13
annita049 <e.annita2001@gmail.com>
2024-09-14 14:22:54 +0600

    clean codes

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -1,19 +1,19 @@
 export const CreateTask = async (req, res)=>{
-
+    res.json({status: "success"});
 }
 
 export const UpdateTaskStatus = async (req, res)=> {
-
+    res.json({status: "success"});
 }
 
 export const TaskListByStatus = async (req, res)=> {
-
+    res.json({status: "success"});
 }
 
 export const DeleteTask = async (req, res)=> {
-
+    res.json({status: "success"});
 }
 
 export const CountTask = async (req, res)=> {
-
+    res.json({status: "success"});
 }
