8ab8d06f80f1f413c103f6e42130798ce2734f14
annita049 <e.annita2001@gmail.com>
2024-09-21 11:27:02 +0600

    imported db_url

--- configs/db_config.js
+++ configs/db_config.js
@@ -1,12 +1,8 @@
 import mongoose from "mongoose";
-
-const DB_URL = "mongodb+srv://annita01:YJVyFga8KJXSkobC@mern.bcxyy.mongodb.net/TaskManager";
+import {DB_URL} from "./config.js"
 
 const DB_connection = ()=> {
-    /**
-     * Connects to the MongoDB database using the provided DB_URL.
-     * This function is exported and intended to be used to establish the database connection.
-     */
+    
     mongoose.connect(DB_URL, {autoIndex: true})
     .then(()=>{
         console.log("DataBase Connection Established");
