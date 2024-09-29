5d297fc272dc9b7325b75d68b5039a7f3610a9aa
annita049 <e.annita2001@gmail.com>
2024-09-29 21:08:24 +0600

    Defined JWT Token Encode-Decode

--- utility/tokenUtility.js
+++ utility/tokenUtility.js
@@ -1,5 +1,13 @@
-export const EncodeToken = async (req, res)=> {
+import { JWT_EXPIRE_TIME, JWT_KEY } from "../configs/config.js";
+import jwt from "jsonwebtoken";
 
+
+export const EncodeToken = (email, user_id)=> {
+    const KEY = JWT_KEY;
+    const EXPIRATION = {expiresIn: JWT_EXPIRE_TIME};
+    const PAYLOAD = {email: email, user_id: user_id};
+
+    return jwt.sign(PAYLOAD, KEY, EXPIRATION);
 }
 
 export const DecodeToken = async (req, res)=> {
