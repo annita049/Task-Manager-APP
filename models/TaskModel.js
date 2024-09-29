969de177ed2472e01e9feb30ed04e4056645abf6
annita049 <e.annita2001@gmail.com>
2024-09-29 21:17:15 +0600

    Changed model name casing

---
+++ models/TaskModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+
+const TaskSchema = new mongoose.Schema(
+    {
+        title: {type: String, required: true},
+        description: {type: String},
+        status: {type: String, required: true},
+        user_id: {type: mongoose.Schema.Types.ObjectId, required: true},
+    },
+    {
+        timestamps: true,
+        versionKey: false
+    }
+);
+
+const Tasks = mongoose.model('tasks','UserSchema');
+
+export default Tasks;
