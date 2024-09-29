6ae3fe7c8eb54e877c23bbb957913220718720de
annita049 <e.annita2001@gmail.com>
2024-09-29 21:12:05 +0600

    implemented Restration and Login with JWT Token

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,9 +1,35 @@
-export const Registration = async (req, res)=> {
-    return res.json({status: "success"});
+import UserModel from "../models/UserModel.js"
+import { EncodeToken } from "../utility/tokenUtility.js";
+
+export const Registration = async (req, res) => {
+    try {
+        const user = req.body;
+        await UserModel.create(user);
+        return res.json({status: "success", message: "User registered successfully!"});
+    }
+    catch(err){
+        return res.json({status: "fail", message: err.toString()});
+    }
 }
 
 export const Login = async (req, res)=> {
-    return res.json({status: "success"});
+    try {
+        const user = req.body;
+        const data = await UserModel.findOne(user);
+        console.log(data);
+        if(data){
+            // Login Successful 
+            // JWT token encode-decode
+            let token = EncodeToken(data.email, data._id);
+            console.log(token);
+            return res.json({status: "success", message: "User found!", data: {token}});
+        }
+        else
+            return res.json({status: "fail", message: "Couldn't find user!"});      
+    }
+    catch(err){
+        return res.json({status: "fail", message: err.message});
+    }
 }
 
 export const ProfileDetails = async (req, res)=> {
