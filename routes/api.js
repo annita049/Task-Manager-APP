edc5a811a561dceed4a3d9abc80fec049c517fc0
annita049 <e.annita2001@gmail.com>
2024-10-12 19:07:54 +0600

    Added AuthMiddlewares within functions that requires Login

--- routes/api.js
+++ routes/api.js
@@ -5,18 +5,19 @@ import * as TaskController from "../controllers/TaskController.js"
 import * as UserController from "../controllers/UserController.js"
 
 import DB_Connection from "../configs/db_config.js";
+import AuthMiddleware from "../middlewares/AuthMiddleware.js";
 
-// Users
+// Users Routes
 
 router.post("/Registration", UserController.Registration);
 router.post("/Login", UserController.Login);
-router.get("/ProfileDetails", UserController.ProfileDetails);
-router.post("/UpdateProfile", UserController.UpdateProfile);
+router.get("/ProfileDetails", AuthMiddleware, UserController.ProfileDetails);
+router.post("/UpdateProfile", AuthMiddleware, UserController.UpdateProfile);
 router.post("/VerifyEmail", UserController.VerifyEmail);
 router.post("/VerifyCode", UserController.VerifyCode);
 router.post("/ResetPassword", UserController.ResetPassword);
 
-// Task
+// Task Routes
 
 router.post("/CreateTask",TaskController.CreateTask);
 router.get("/UpdateTaskStatus",TaskController.UpdateTaskStatus);
