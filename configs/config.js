b8df0ac6f99c5e497b5463434a1fa3549f9a6d57
annita049 <e.annita2001@gmail.com>
2024-10-13 15:02:15 +0600

    removed all confidential info from config file

--- configs/config.js
+++ configs/config.js
@@ -1,13 +1,21 @@
-export const PORT = 9050;
+import dotenv from 'dotenv';
+dotenv.config();
 
-export const DB_URL = "mongodb+srv://annita01:YJVyFga8KJXSkobC@mern.bcxyy.mongodb.net/TaskManager";
+// PORT
+export const PORT =  process.env.PORT;
 
-export const JWT_KEY = "ABC12341241234vb";
-export const JWT_EXPIRE_TIME = "30d";
+// MongoDB Connection String
+export const DB_URL = process.env.DB_URL;
 
-export const EMAIL_PORT = 25;
-export const EMAIL_SECURITY = false;
+// JWT Configuration
+export const JWT_KEY = process.env.JWT_KEY;
+export const JWT_EXPIRE_TIME = process.env.JWT_EXPIRE_TIME;
+
+// Email Provider Configuration
 export const EMAIL_UNAUTHORIZED = false;
+export const EMAIL_USER = process.env.EMAIL_USER;
+export const EMAIL_PASS = process.env.EMAIL_PASS;
+export const EMAIL_SECURITY = process.env.EMAIL_SECURITY;
 
 export const WEB_CACHE = false;
 export const MAX_JSON_SIZE = "10MB";
