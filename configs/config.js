f71749cd15217bb632e4536af2d0e02aaa874a7c
annita049 <e.annita2001@gmail.com>
2024-10-13 13:35:26 +0600

    removed all confidential info from config file

--- configs/config.js
+++ configs/config.js
@@ -5,11 +5,8 @@ export const DB_URL = "mongodb+srv://annita01:YJVyFga8KJXSkobC@mern.bcxyy.mongod
 export const JWT_KEY = "ABC12341241234vb";
 export const JWT_EXPIRE_TIME = "30d";
 
-export const EMAIL_HOST = "mail.teamrabbil.com";
 export const EMAIL_PORT = 25;
 export const EMAIL_SECURITY = false;
-// export const EMAIL_USER = "info@teamrabbil.com";
-// export const EMAIL_PASS = "~sR4[bhaC[Qs";
 export const EMAIL_UNAUTHORIZED = false;
 
 export const WEB_CACHE = false;
