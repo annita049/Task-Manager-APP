f2dfee83f2d24df045aac047996871beea3671e6
annita049 <e.annita2001@gmail.com>
2024-10-14 14:14:33 +0600

    updated all the functions and specially emailverify and sendotp

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,31 +1,51 @@
 import bcrypt from 'bcrypt'
+import crypto from 'crypto'
 import UserModel from "../models/UserModel.js"
 import SendEmail from "../utility/emailUtility.js";
 import { EncodeToken } from "../utility/tokenUtility.js";
+
+const generateOTP = ()=> {
+    return crypto.randomInt(100000, 999999);  // 6-digit OTP in string
+}
+
+export const SendOTP = async(user)=>{
+    const otp = generateOTP();
+    const otpExpiration = new Date(Date.now() + 10*60*1000);
+
+    // saving otp to database
+    user.otp = otp;
+    user.otpExpiration = otpExpiration;
+    await user.save();
+
+    const EmailTo = user.email;
+    const EmailSubject = "Task Manager App Email Verification";
+    const EmailContent = `Your OTP for email verification is ${otp}`;
+    await SendEmail(EmailTo, EmailSubject, EmailContent);
+}
 
 export const Registration = async (req, res) => {
     try {
         let {email, firstname, lastname, password} = req.body;
         let user = await UserModel.findOne({email});
         if(user){
-            return res.status(400).json({status: "success", message: "User already exists!"});
+            return res.status(400).json({status: "fail", message: "User already exists!"});
         }
-
-        // Hashing of plain password --> bcrypt
-        
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-        console.log(hashedPassword);
+        // const otp = generateOTP();
+        // console.log("my otp---->", otp);
 
         user = new UserModel({
             email,
             firstname,
             lastname,
-            password: hashedPassword
+            password,
+            // otp,
+            // otpExpiration: Date.now() + 10 * 60 * 1000
         });
-
         await user.save();
-        return res.status(200).json({status: "success", message: "User registered successfully!"});
+
+        await SendOTP(user);
+
+        return res.status(201).json({status: "success", message: "User registered successfully! OTP sent to email for verification."});
     }
     catch(err){
         res.status(500).json({status: "fail", message: err.toString()});
@@ -40,111 +60,115 @@ export const Login = async (req, res)=> {
         if(!user){
             return res.status(400).json({status:"fail", message: "User not found!" });
         }
+
+        // if (!user.isVerified) {
+        //     return res.status(401).json({status: "fail", message: 'Email not verified. Please verify your email first' });
+        // }
+
         const isMatch = await bcrypt.compare(req.body.password, user.password);
         if(!isMatch){
-            // Login Successful 
-            // JWT token encode-decode
             return res.status(400).json({status:"fail", message: 'Wrong password'});
         }
         let token = EncodeToken(user.email, user._id);
         // console.log(token);
-        return res.status(200).json({status: "success", message: "User found!", data: {token}});
+        res.status(200).json({status: "success", message: "Login Successful, user found!", user, token});
     }
     catch(err){
         res.status(500).json({status: "fail", message: err.toString()});
     }
 }
 
-
 export const ProfileDetails = async (req, res)=> {
 
     try {
-        let user = await UserModel.findOne({_id: req.user.user_id}).select(['firstname', 'lastname', 'email']);
+        const user_id = req.user.user_id;
+        let user = await UserModel.findById(user_id).select(['firstname', 'lastname', 'email']);
 
-        if (!user) return res.status(404).send('User not found');
-        res.status(200).json({status: "success", user});
+        if (!user)
+            return res.status(404).json({status: "fail", message: "User Not found!"});
+
+        res.status(200).json({status: "success", message: "User found!",user});
     }
     catch(e){
-        res.status(500).json({status: "success", user});
+        res.status(500).json({status: "fail", message: e.toString()});
     }
 }
 
 export const UpdateProfile = async (req, res)=> {
     try{
-        let user_id = req.user.user_id;
-        let data = req.body;
-        await UserModel.updateOne({_id: user_id}, data);
-        return res.json({status: "successfully updated data"});
+        const user_id = req.user.user_id;
+        const data = req.body;
+        const user = await UserModel.findById(user_id);
+
+        if (!user) 
+            return res.status(404).json({status: "fail", message: "User Not found!"});
+
+        // data is client provided info
+        // user is header attached by auth middleware
+        user.firstname = data.firstname || user.firstname;
+        user.lastname = data.lastname || user.lastname;
+        user.password = data.password || user.password;
+
+        await user.save();
+        res.json({status: "success", message: "successfully updated data", user});
     }
     catch(e){
-        res.json(e.toString());
+        res.status(500).json({status: "fail", message: e.toString()});
     }
 }
 
 export const VerifyEmail = async (req, res)=> {
+    const {otp} = req.body; // only OTP is provided by the client
+
     try {
-        let email = req.params.email;
-        let user = await UserModel.findOne({email: email});
-    
+        // email is already known as the user is authenticated
+        const email = req.user.email;
+        const user = await UserModel.findOne({email});
         if (!user) {
-            res.json({status: "fail", message: "User Does not exist!"});
+            return res.status(400).json({status: 'fail', message: 'User email not found' });
         }
-        else {
 
-            // Send verifitcation OTP in User Email
-            let EmailTo = user.email;
-            let EmailSubject = "Task Manager App User Email Verification Code";
-            let otp = Math.floor(100000 + Math.random() * 900000);
-            let EmailText = "Your Verification OTP is " + otp;
+        // if (user.otp !== otp) {
+        //     return res.status(400).json({status: 'fail', message: 'Invalid OTP'});
+        // }
 
-            SendEmail(EmailTo, EmailSubject, EmailText);
-
-            // Update User OTP
-            await UserModel.updateOne({email: email}, {otp})
-            res.json({status: "success", message: "OTP sent to user Email"});
+        // if the OTP is expired a new OTP is sent to user email
+        if(user.otpExpiration < Date.now()){
+            await SendOTP(user);
+            return res.status(400).json({status: 'fail', message: 'OTP Expired'});
         }
-    }
-    catch(e){
-        res.json({status: "fail", message: e.toString()});
-    }
-}
 
+        // if OTP is valid --> user is verified
+        user.isEmailVerified = true;
+        user.otp = null;
+        user.otpExpiration = null;
+        await user.save();
 
-export const VerifyCode = async (req, res)=> {
-    try {
-        let user = await UserModel.findOne({email: req.body.email, otp: req.body.otp});
-        // console.log("NPNP", user);
-        if(!user){
-            return res.json({status: "fail", message: "Wrong Verification Code"});
-        }
-        res.json({status: "success", message: "Verification Successful"});
-
+        res.status(200).json({status: 'success', message: 'Email verification successful' });
     }
-    catch(e){
-        res.json({status: "fail", message: e.toString()});
+    catch (e) {
+        res.status(500).json({status: 'fail', message: e.toString()});
     }
 }
 
-export const ResetPassword = async (req, res)=> {
+export const RequestPasswordReset = async (req, res)=> {
+
+    // email is provided by the client
+    const {email} = req.body;
+
     try {
-        let user = await UserModel.findOne({email: req.body.email, otp: req.body.otp});
-        // console.log("NPNP", user);
-        if(!user){
-            res.json({status: "fail", message: "Wrong Verification Code"});
+        const user = await UserModel.findOne({email});
+        if (!user) {
+            return res.status(404).json({status: "fail", message: "Email not found"});
         }
-        else {
-            await UserModel.updateOne(
-                {email: req.body.email},
-                {
-                    otp: "0",
-                    password: req.body.password
-                }
-            )
-            res.json({status: "success", message: "Password Reset Successful"});
-        }
+
+        // Send the OTP via email (use your email service)
+        await SendOTP(user);
+
+        res.status(200).json({status: "success", message: "Password reset OTP sent to your email"});
     }
-    catch(e){
-        res.json({status: "fail", message: e.toString()});
+    catch (err) {
+        res.status(500).json({status: "success", message: e.toString()});
     }
 }
 
