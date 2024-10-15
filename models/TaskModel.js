a6acde0d49a84ae4433a39df230c9cdab31df21a
annita049 <e.annita2001@gmail.com>
2024-10-15 19:55:58 +0600

    Make the description field take default value as null

--- models/TaskModel.js
+++ models/TaskModel.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose"
 const TaskSchema = new mongoose.Schema(
     {
         title: {type: String, required: true},
-        description: {type: String},
+        description: {type: String, default: ""},
         status: {type: String, default: "Pending"},
         priority: {type: String, required: true},
-        user_id: {type: mongoose.Schema.Types.ObjectId, required: true},
+        user_id: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
     },
     {
         timestamps: true,
@@ -14,6 +14,6 @@ const TaskSchema = new mongoose.Schema(
     }
 );
 
-const Tasks = mongoose.model('tasks','UserSchema');
+const Task = mongoose.model('Task', TaskSchema);
 
-export default Tasks;
+export default Task;
